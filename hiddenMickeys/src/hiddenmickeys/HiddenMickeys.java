1c6c3f6b5a41738773b4e944addf800d042f2254
Administrator <Administrator@192.168.0.6>
2016-05-13 14:02:36 -0600

    Adding FastPassStation, Game, Shop classes. Modified HiddenMickeys.java

--- hiddenMickeys/src/hiddenmickeys/HiddenMickeys.java
+++ hiddenMickeys/src/hiddenmickeys/HiddenMickeys.java
@@ -14,6 +14,9 @@ import byui.cit260.hiddenMickeys.model.Player;
 import byui.cit260.hiddenMickeys.model.Character;
 import byui.cit260.hiddenMickeys.model.Backpack;
 import byui.cit260.hiddenMickeys.model.Map;
+import byui.cit260.hiddenMickeys.model.Game;
+import byui.cit260.hiddenMickeys.model.FastPassStation;
+import byui.cit260.hiddenMickeys.model.Shop;
 
 public class HiddenMickeys {
 
@@ -57,6 +60,33 @@ public class HiddenMickeys {
         
         String mapInfo = playerMap.toString();
         System.out.println(mapInfo);
+        
+        
+        Game myGame = new Game();
+        myGame.setCurrentColumn(1);
+        myGame.setCurrentRow(4);
+        myGame.setEnergyLevel(90);
+        myGame.setMickeysCollected(4);
+        myGame.setTimeExpired(160);
+        myGame.setTimeRemaining(600-myGame.getTimeExpired());
+        
+        String gameInfo = myGame.toString();
+        System.out.println(gameInfo);
+        
+        FastPassStation station1 = new FastPassStation();
+        station1.setLastVisitedTime(100);
+        station1.setNextAvailableTime((station1.getLastVisitedTime()+45));
+        
+        String fastPassStationInfo = station1.toString();
+        System.out.println(fastPassStationInfo);
+        
+        Shop shop1 = new Shop();
+        shop1.setCost(2.25);
+        shop1.setMickeyAvailable(true);
+        shop1.setProductName("Blue Mickey Key Chain");
+        
+        String shopInfo = shop1.toString();
+        System.out.println(shopInfo);
     }
     
 }
