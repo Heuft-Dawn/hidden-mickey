2e4f0dbf9ef7ec609d64feb07ee6fe8923b86fa9
Suzy Allen <Administrator@192.168.0.9>
2016-07-08 09:25:53 -0600

    added comments to LocationsVisitedReportView code

--- hiddenMickeys/src/byui/cit260/hiddenMickeys/view/LocationVisitedReportView.java
+++ hiddenMickeys/src/byui/cit260/hiddenMickeys/view/LocationVisitedReportView.java
@@ -52,36 +52,46 @@ public class LocationVisitedReportView extends View {
       String filePath = this.getInput();
       
         try (PrintWriter out = new PrintWriter(filePath)) {
+            //Write the report header
             out.println("\nLOCATIONS VISITED REPORT");
             out.printf("%n%-5s%-35s%-15s","Loc#","Location Name","Mickey Found?");
             out.printf("%n%-5s%-35s%-15s","-----","-----------------------------------","-------------");
+            
+            //pull the data
             Game game = HiddenMickeys.getCurrentGame();
             Location[][] locations = game.getMap().getLocations();
             Mickey[] mickeys = game.getBackpack().getMickeysCollected();
+            //initialize needed variables 
             boolean mickeyFound;
             String mickeyFoundDisplay;
             int curLocationNum;
+            
+            //loop through all the locations
             for (int row = 0; row < locations.length; row++){
                 for( int column = 0; column < locations[row].length; column++){ 
                     curLocationNum = locations[row][column].getLocationNo();
                     //this.console.println(Integer.toString(curLocationNum));
                     mickeyFound = false;
                     mickeyFoundDisplay = "N";
+                    //if the location has been visited, search the Mickeys collected to check for a match
                     if(locations[row][column].isVisited()){
                                            
                            for (Mickey mickey: mickeys){
                            //this.console.println(Integer.toString(mickey.getLocationNum()));
+                           //if there is a matching mickey location, set mickeyfound to true
                            if (mickey.getLocationNum() == curLocationNum){
                                mickeyFound=true;
                             }
                            }
                     if(mickeyFound){mickeyFoundDisplay = "Y";}       
+                    //display the location in the report with the correct Mickey status
                     out.printf("%n%-5s%-35s%-15s",Integer.toString(curLocationNum),locations[row][column].getScene().getName(), mickeyFoundDisplay);
                     
                     }
                  }
             }
             out.println("\n\n ");
+            //close the file
             try{
                 if (out != null)
                  out.close();
@@ -100,30 +110,37 @@ public class LocationVisitedReportView extends View {
     }
 
     private void viewReport() {
+        //report header
             this.console.println("\nLOCATIONS VISITED REPORT");
             this.console.printf("%n%-5s%-35s%-15s","Loc#","Location Name","Mickey Found?");
             this.console.println("\n-------------------------------------------------------------");
+        //get the data
             Game game = HiddenMickeys.getCurrentGame();
             Location[][] locations = game.getMap().getLocations();
             Mickey[] mickeys = game.getBackpack().getMickeysCollected();
+        //initialize needed variables
             boolean mickeyFound;
             String mickeyFoundDisplay;
             int curLocationNum;
+        //loop through the locations to find visited ones
             for (int row = 0; row < locations.length; row++){
                 for( int column = 0; column < locations[row].length; column++){ 
                     curLocationNum = locations[row][column].getLocationNo();
                     //this.console.println(Integer.toString(curLocationNum));
                     mickeyFound = false;
                     mickeyFoundDisplay = "N";
+                   //if the location has been visited, check to see if a mickey was found there
                     if(locations[row][column].isVisited()){
-                                           
+                          //search the mickey array                 
                            for (Mickey mickey: mickeys){
                            //this.console.println(Integer.toString(mickey.getLocationNum()));
                            if (mickey.getLocationNum() == curLocationNum){
                                mickeyFound=true;
                             }
                            }
+                    //set the mickeyFoundDisplay value for the report       
                     if(mickeyFound){mickeyFoundDisplay = "Y";}       
+                    //print the visited location on the report
                     this.console.printf("%n%-5s%-35s%-15s",Integer.toString(curLocationNum),locations[row][column].getScene().getName(), mickeyFoundDisplay);
                     
                     }
